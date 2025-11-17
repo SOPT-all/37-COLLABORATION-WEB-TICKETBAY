a0e8cd3799a2a2df00575da97375a161d028defc
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-18 00:58:54 +0900

    fix : gcTime 설정 및 컨벤션 규칙 반영

--- src/shared/apis/config/query-provider.tsx
+++ src/shared/apis/config/query-provider.tsx
@@ -3,11 +3,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./query-client";
 
-interface QueryProviderProps {
+interface Props {
   children: ReactNode;
 }
 
-export function QueryProvider({ children }: QueryProviderProps) {
+export function QueryProvider({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}

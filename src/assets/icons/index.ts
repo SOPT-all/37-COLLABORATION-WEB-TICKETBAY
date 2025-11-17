c480791cfda2e6f1d1e7199e17783ce5aff76a39
jeonghoon11 <jih361@naver.com>
2025-11-17 16:03:13 +0900

    chore: 아이콘 import 예시 삭제

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,5 @@
 // 아이콘 배럴 파일
 // 실제 아이콘이 생기면 아래처럼 개별 export 로 추가해서 사용하시면 됩니다.
-// 예시:
-// export { default as TimeIcon } from "@assets/svg/time.svg?react";
-// export { default as SearchIcon } from "@assets/svg/search.svg?react";
 
 export { default as MusicalIcon } from "@assets/svg/icon_ musical.svg?react";
 export { default as CartIcon } from "@assets/svg/icon_cart.svg?react";

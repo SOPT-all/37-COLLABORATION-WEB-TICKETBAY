8deb49a50a96a2558be9ca6556eb205b41fb5a31
hello-yujin <yuuuujiiiin@naver.com>
2025-11-21 04:31:46 +0900

    refactor: CategoryIcon `onClick` props 제거

--- src/shared/components/category-icon/category-icon.tsx
+++ src/shared/components/category-icon/category-icon.tsx
@@ -6,12 +6,11 @@ interface Props {
   icon: ReactNode;
   text: string;
   isActive?: boolean;
-  onClick?: () => void;
 }
 
-const CategoryIcon = ({ icon, text, isActive = false, onClick }: Props) => {
+const CategoryIcon = ({ icon, text, isActive = false }: Props) => {
   return (
-    <button type="button" className={styles.container} onClick={onClick}>
+    <button type="button" className={styles.container}>
       <div className={styles.icon({ isActive })}>{icon}</div>
       <p className={styles.text({ isActive })}>{text}</p>
     </button>

2e54ce336c0ff28a0178b30ff7ccf14979196c98
jeonghoon11 <jih361@naver.com>
2025-11-21 23:31:47 +0900

    feat: navigation에 뒤로가기 action 추가

--- src/pages/select-ticket/select-ticket.tsx
+++ src/pages/select-ticket/select-ticket.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router";
+
 import { ChevronBigLeftIcon, InfoIcon } from "@assets/icons";
 
 import BottomSheet from "@widgets/select-ticket/components/bottom-sheet/bottom-sheet";
@@ -6,12 +8,19 @@ import Header from "@widgets/select-ticket/components/header/header";
 import Navigation from "@shared/components/navigation/navigation";
 
 const SelectTicket = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToTicketDetail = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <Navigation
         title="한국시리즈 5차전 [대전]"
         leftIcon={<ChevronBigLeftIcon width={24} height={24} />}
         rightIcon={<InfoIcon width={24} height={24} />}
+        leftAction={handleNavigateToTicketDetail}
       />
       <Header eventTitle="한국시리즈 5차전 [대전]" stadiumName="한화생명볼파크" />
       <BottomSheet />

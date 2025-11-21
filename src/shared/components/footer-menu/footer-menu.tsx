fff83ba26ef254db591d7bf213b89231b4d16c95
Sohyunnnn <thgus05061@naver.com>
2025-11-21 22:15:10 +0900

    rename: 네이밍 변경

--- src/shared/components/footer-menu/footer-menu.tsx
+++ src/shared/components/footer-menu/footer-menu.tsx
@@ -1,17 +1,17 @@
 import * as styles from "./footer-menu.css";
 
-const menuItem = ["서비스 소개", "이용 가이드", "공지사항", "고객센터"];
+const menuItems = ["서비스 소개", "이용 가이드", "공지사항", "고객센터"];
 
 const FooterMenu = () => {
   return (
     <nav className={styles.container}>
       <ul className={styles.ul}>
-        {menuItem.map((item, index) => (
+        {menuItems.map((item, index) => (
           <li key={item} className={styles.li}>
             <button type="button" className={styles.button}>
               {item}
             </button>
-            {index !== menuItem.length - 1 && <div className={styles.divider} />}
+            {index !== menuItems.length - 1 && <div className={styles.divider} />}
           </li>
         ))}
       </ul>

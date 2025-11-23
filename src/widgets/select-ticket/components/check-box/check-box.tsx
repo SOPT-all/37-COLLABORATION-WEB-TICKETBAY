9f1fe473d1615611b18bf7b380575caf3f1c1e8e
jeonghoon11 <jih361@naver.com>
2025-11-23 12:55:50 +0900

    feat: check-box-label 제거 및 props로 추가

--- src/widgets/select-ticket/components/check-box/check-box.tsx
+++ src/widgets/select-ticket/components/check-box/check-box.tsx
@@ -4,19 +4,28 @@ import { CheckBoxIcon, CheckedBoxIcon } from "@assets/icons";
 
 import * as styles from "./check-box.css";
 
-const CheckBox = () => {
+interface Props {
+  label?: string;
+}
+
+const CheckBox = ({ label }: Props) => {
   const [isChecked, setIsChecked] = useState(false);
   const handleCheckBoxClick = () => {
     setIsChecked((prev) => !prev);
   };
 
   return (
-    <button type="button" onClick={handleCheckBoxClick}>
-      {isChecked && (
-        <CheckedBoxIcon width={14} height={14} className={styles.icon({ isChecked })} />
-      )}
-      {!isChecked && <CheckBoxIcon width={14} height={14} className={styles.icon({ isChecked })} />}
-    </button>
+    <div className={styles.container}>
+      <button type="button" onClick={handleCheckBoxClick}>
+        {isChecked && (
+          <CheckedBoxIcon width={14} height={14} className={styles.icon({ isChecked })} />
+        )}
+        {!isChecked && (
+          <CheckBoxIcon width={14} height={14} className={styles.icon({ isChecked })} />
+        )}
+      </button>
+      {label && <span className={styles.label}>{label}</span>}
+    </div>
   );
 };
 

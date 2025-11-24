b5caa5c8f86757706eff31c04a56e7d3b81d487e
Sohyunnnn <thgus05061@naver.com>
2025-11-24 19:44:40 +0900

    style: marginTop 추가

--- src/widgets/home/components/best-ticket/best-ticket.tsx
+++ src/widgets/home/components/best-ticket/best-ticket.tsx
@@ -3,21 +3,25 @@ import Tab from "@shared/components/tab/tab";
 import Section from "../section/section";
 import BestTicketList from "./best-ticket-list/best-ticket-list";
 
+import * as styles from "./best-ticket.css";
+
 const BestTicket = () => {
   return (
     <Section title="베스트" highlight="인기티켓">
-      <Tab.Container initialValue="0">
-        <Tab.List>
-          <Tab.Item value="0">1~10위</Tab.Item>
-          <Tab.Item value="1">11~20위</Tab.Item>
-        </Tab.List>
-        <Tab.Panel value="0">
-          <BestTicketList page={0} />
-        </Tab.Panel>
-        <Tab.Panel value="1">
-          <BestTicketList page={1} />
-        </Tab.Panel>
-      </Tab.Container>
+      <div className={styles.container}>
+        <Tab.Container initialValue="0">
+          <Tab.List>
+            <Tab.Item value="0">1~10위</Tab.Item>
+            <Tab.Item value="1">11~20위</Tab.Item>
+          </Tab.List>
+          <Tab.Panel value="0">
+            <BestTicketList page={0} />
+          </Tab.Panel>
+          <Tab.Panel value="1">
+            <BestTicketList page={1} />
+          </Tab.Panel>
+        </Tab.Container>
+      </div>
     </Section>
   );
 };

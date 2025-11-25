adc1b48f8a6bc1ac57e52ecd88e9cd54eaa476a4
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-26 04:54:55 +0900

    feat: 홈 페이지에 최근 본 티켓 섹션 추가
    - 홈 페이지에 RecentTicket 컴포넌트 추가

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,20 @@
 import Header from "@widgets/home/components/header/header";
 import Search from "@widgets/home/components/search/search";
+import RecentTicket from "@widgets/home/components/recent-ticket/recent-ticket";
 
 import * as styles from "./home.css";
 
 function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.backgroundImage} />
-      <div className={styles.gradient} />
-      <Header />
-      <Search />
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.backgroundImage} />
+        <div className={styles.gradient} />
+        <Header />
+        <Search />
+      </div>
+      <RecentTicket />
+    </>
   );
 }
 

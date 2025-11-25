b49cef99cb1b33f8f3b035d48c6b0c9c1863413b
Sohyunnnn <thgus05061@naver.com>
2025-11-25 14:34:28 +0900

    refactor: 코드 개선

--- src/widgets/home/components/contact/contact.tsx
+++ src/widgets/home/components/contact/contact.tsx
@@ -1,17 +1,15 @@
-import {
-  CUSTOMER_SERVICE_LUNCH_BREAK,
-  CUSTOMER_SERVICE_OPERATING_HOURS,
-  CUSTOMER_SERVICE_TEL,
-} from "@widgets/home/constants/contact";
+import { CUSTOMER_SERVICE } from "@widgets/home/constants/contact";
 
 import * as styles from "./contact.css";
 
 const Contact = () => {
+  const { TEL, OPERATING_HOURS, LUNCH_BREAK } = CUSTOMER_SERVICE;
+
   return (
     <section className={styles.container}>
-      <p className={styles.telephone}>고객센터 | {CUSTOMER_SERVICE_TEL}</p>
-      <p className={styles.text}>운영시간: {CUSTOMER_SERVICE_OPERATING_HOURS}</p>
-      <p className={styles.text}>점심시간: {CUSTOMER_SERVICE_LUNCH_BREAK}</p>
+      <p className={styles.telephone}>고객센터 | {TEL}</p>
+      <p className={styles.text}>운영시간: {OPERATING_HOURS}</p>
+      <p className={styles.text}>점심시간: {LUNCH_BREAK}</p>
     </section>
   );
 };

ba313fc11a4dc3408ed4ba2c1d50cc0c9cd7b9ed
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-26 04:59:47 +0900

    fix: 홈 페이지에서 RecentTicket 컴포넌트 사용 누락 수정
    - RecentTicket import는 되어있으나 JSX에서 사용되지 않던 문제 수정

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,15 +6,15 @@ import * as styles from "./home.css";
 
 function Home() {
   return (
-
+    <>
       <div className={styles.container}>
         <div className={styles.backgroundImage} />
         <div className={styles.gradient} />
         <Header />
         <Search />
       </div>
-
-  
+      <RecentTicket />
+    </>
   );
 }
 

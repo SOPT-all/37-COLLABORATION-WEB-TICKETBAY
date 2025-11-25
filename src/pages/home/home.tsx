d6417dcf567e1a57927dfb45e61efce56fa8efb0
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-26 03:35:50 +0900

    feat: 사이드바 열기/닫기 기능 추가

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Header from "@widgets/home/components/header/header";
+import Sidebar from "@widgets/home/components/sidebar/sidebar";
 
 import * as styles from "./home.css";
 
 function Home() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const handleSidebarClose = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.backgroundImage} />
       <div className={styles.gradient} />
-      <Header />
+      <Header onMenuClick={handleMenuClick} />
+      <Sidebar isOpen={isSidebarOpen} onClose={handleSidebarClose} />
     </div>
   );
 }

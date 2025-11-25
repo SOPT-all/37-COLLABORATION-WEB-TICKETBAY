f86da6eeaf3af5bc62548e817143f5ef65e1179c
Sohyunnnn <thgus05061@naver.com>
2025-11-26 01:19:31 +0900

    feat: 브랜드 컴포넌트 구현

---
+++ src/widgets/home/components/brand/brand.css.ts
@@ -0,0 +1,23 @@
+import { style } from "@vanilla-extract/css";
+
+import { themeVars } from "@shared/styles/theme.css";
+
+export const section = style({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+export const logoContainer = style({
+  display: "flex",
+  gap: "0.5rem",
+  alignItems: "center",
+});
+
+export const logo = style({
+  color: themeVars.color.grayscale1,
+});
+
+export const snsContainer = style({
+  display: "flex",
+  gap: "0.8rem",
+});

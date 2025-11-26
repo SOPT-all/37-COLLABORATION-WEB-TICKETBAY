c00cae5bf7f4b34b5dc1427d31eced6474732479
hello-yujin <yuuuujiiiin@naver.com>
2025-11-26 17:22:24 +0900

    feat: SeatMap 컴포넌트 구현 및 URL Params 기반 이미지 렌더링 추가

---
+++ src/widgets/ticket-detail/components/seat-map/seat-map.tsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+
+import { getSeatMapById } from "@widgets/ticket-detail/constants/seat-map.mock";
+
+import * as styles from "./seat-map.css";
+
+type RouteParams = {
+  ticketId?: string;
+};
+
+const SeatMap = () => {
+  const { ticketId } = useParams<RouteParams>();
+
+  const numericId = Number(ticketId);
+  const seatImageUrl =
+    ticketId && !Number.isNaN(numericId) ? getSeatMapById(numericId)?.seatImageUrl : undefined;
+
+  const imageSrc = seatImageUrl ?? "/img_seatview_118.webp";
+
+  return <img src={imageSrc} alt="좌석 배치도" className={styles.seatImage} />;
+};
+
+export default SeatMap;

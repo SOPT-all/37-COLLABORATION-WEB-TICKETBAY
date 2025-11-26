36eab7f1841258dc50ebcc3483b046331b2f7978
Sohyunnnn <thgus05061@naver.com>
2025-11-26 21:17:09 +0900

    style: 스타일 수정

--- src/widgets/home/components/review/review.css.ts
+++ src/widgets/home/components/review/review.css.ts
@@ -5,7 +5,7 @@ export const container = style({
   display: "flex",
   gap: "1.2rem",
   overflowX: "auto",
-  marginRight: "-1.4rem",
-  paddingRight: "1.4rem",
+  marginRight: "-1.5em",
+  paddingRight: "1.5rem",
   WebkitOverflowScrolling: "touch",
 });

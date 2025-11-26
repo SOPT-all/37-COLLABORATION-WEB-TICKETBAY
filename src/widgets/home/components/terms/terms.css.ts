018713ddf9f1085ad7015e603361efcd3f7c8451
Sohyunnnn <thgus05061@naver.com>
2025-11-27 02:46:38 +0900

    style: 스타일 수정

--- src/widgets/home/components/terms/terms.css.ts
+++ src/widgets/home/components/terms/terms.css.ts
@@ -27,6 +27,7 @@ export const container = recipe({
 });
 
 export const button = style({
+  ...themeVars.fontStyles.caption1_regular
   width: "100%",
   padding: "0.7rem 1rem",
   background: themeVars.color.grayscale9,

283c76005c14ee55e1d4f0537fb523a4202f691b
Sohyunnnn <thgus05061@naver.com>
2025-11-26 20:41:13 +0900

    feat: 이용 후기  컴포넌트 구현

---
+++ src/widgets/home/components/review/review.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  marginTop: "0.6rem",
+  display: "flex",
+  gap: "1.2rem",
+  overflowX: "auto",
+  marginRight: "-1.4rem",
+  paddingRight: "1.4rem",
+  WebkitOverflowScrolling: "touch",
+});

56c435dc64b30a55dc5525728f94eb1d32a52477
hello-yujin <yuuuujiiiin@naver.com>
2025-11-27 07:37:02 +0900

    feat: 하단 버튼 (BottomButtons) 섹션 UI 구현

---
+++ src/widgets/ticket-detail/components/bottom-buttons/bottom-buttons.tsx
@@ -0,0 +1,35 @@
+import { TicketbayGlobalButton } from "@widgets/home/components/TicketbayGlobalButton/TicketbayGlobalButton";
+import { BuyButton } from "@widgets/ticket-detail/components/buy-button/buy-button";
+
+import CompareAddButton from "@shared/components/compare-add-button/compare-add-button";
+import FooterMenu from "@shared/components/footer-menu/footer-menu";
+
+import * as styles from "./bottom-buttons.css";
+
+const BottomButtons = () => {
+  return (
+    <section className={styles.container}>
+      <FooterMenu />
+
+      <p className={styles.notice}>
+        티켓베이는 통신판매 중개자이며, 통신판매의 당사자가 아닙니다. <br /> 따라서 티켓베이는 및
+        거래에 대하여 책임을 지지 않습니다.
+      </p>
+
+      <div className={styles.globalButtonWrapper}>
+        <TicketbayGlobalButton />
+      </div>
+
+      <div className={styles.buttonRow}>
+        <div className={styles.compareButtonWrapper}>
+          <CompareAddButton size="lg" />
+        </div>
+        <div className={styles.buyButtonWrapper}>
+          <BuyButton />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BottomButtons;

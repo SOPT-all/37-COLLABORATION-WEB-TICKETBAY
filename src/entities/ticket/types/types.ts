0aedc059d62d4a4f3037ddf0960755bf8e4e82c5
hello-yujin <yuuuujiiiin@naver.com>
2025-11-27 16:48:52 +0900

    fix: 티켓 타입(TicketDetail) 스키마 최신화

--- src/entities/ticket/types/types.ts
+++ src/entities/ticket/types/types.ts
@@ -9,3 +9,26 @@ export type GetOptionCardResponse =
   paths["/events/{eventId}/tickets"]["get"]["responses"]["200"]["content"]["*/*"];
 
 export type TicketResponse = components["schemas"]["TicketResponse"];
+
+export type GetTicketDetailRequest = paths["/tickets/{ticketId}"]["get"]["parameters"]["path"];
+
+export type GetTicketDetailResponse =
+  paths["/tickets/{ticketId}"]["get"]["responses"]["200"]["content"]["*/*"];
+
+type RawEventResponse = components["schemas"]["EventResponse"];
+type RawSeatResponse = components["schemas"]["SeatResponse"];
+
+export type EventDetail = Required<RawEventResponse>;
+export type SeatDetail = Required<RawSeatResponse>;
+
+export interface TicketDetail {
+  id: number;
+  productNumber: number;
+  event: EventDetail;
+  seat: SeatDetail;
+  status: boolean;
+  pricePerTicket: number;
+  amount: number;
+  totalPrice: number;
+  createdAt: string;
+}

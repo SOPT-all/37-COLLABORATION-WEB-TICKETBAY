8811ccb00fd9f637dd0c7c455f4976463786f7d8
Sohyunnnn <thgus05061@naver.com>
2025-11-27 14:47:10 +0900

    refactor: formatDateToDot 적용

--- src/entities/history/ui/recent-ticket-card/recent-ticket-card.tsx
+++ src/entities/history/ui/recent-ticket-card/recent-ticket-card.tsx
@@ -2,6 +2,8 @@ import type { HistoryResponse } from "@entities/history/types/type";
 
 import { ChevronSmallRightIcon } from "@assets/icons";
 
+import { formatDateToDot } from "@shared/utils/format-date-to-dot";
+
 import * as styles from "./recent-ticket-card.css";
 
 interface Props extends HistoryResponse {
@@ -14,7 +16,7 @@ const RecentTicketCard = ({ name, eventDate, onClick }: Props) => {
       <div className={styles.cardInner}>
         <div className={styles.content}>
           <p className={styles.title}>{name}</p>
-          <p className={styles.date}>경기 일시 : {eventDate}</p>
+          <p className={styles.date}>경기 일시 : {formatDateToDot(eventDate!)}</p>
         </div>
         <ChevronSmallRightIcon width={20} height={20} className={styles.icon} />
       </div>

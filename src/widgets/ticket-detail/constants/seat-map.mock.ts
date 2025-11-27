a95d3eb3e9445ffd6e8f5a4bc2934e0b0c55ad81
hello-yujin <yuuuujiiiin@naver.com>
2025-11-27 10:56:52 +0900

    chore: 사용하지 않는 SeatMap 목업 데이터 삭제

--- src/widgets/ticket-detail/constants/seat-map.mock.ts
+++ src/widgets/ticket-detail/constants/seat-map.mock.ts
@@ -2,9 +2,3 @@ export type SeatMapMock = {
   id: number;
   seatImageUrl: string;
 };
-
-export const SEAT_MAP_MOCK_LIST: SeatMapMock[] = [
-  { id: 1, seatImageUrl: "/img_seatview_118.webp" },
-  { id: 2, seatImageUrl: "/img_seatview_410.webp" },
-  { id: 3, seatImageUrl: "/img_seatview_507.webp" },
-];
